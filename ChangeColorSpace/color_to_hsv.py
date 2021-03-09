fc4ffba629a7eb64bf45d0c503bafa8502efa3d2
Avishake Maji <56298861+Avishake007@users.noreply.github.com>
2021-03-09 12:46:40 +0530

    Create color_to_hsv.py

---
+++ ChangeColorSpace/color_to_hsv.py
@@ -0,0 +1,12 @@
+import cv2
+path=input("Enter the path : ") 
+#Read the image
+image = cv2.imread(path)
+#Convert the color image to Gray Scale image
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+#Displaying of Gray iamge
+cv2.imshow('HSV image', gray)
+#Saving of image
+cv2.imwrite("gray_scale.png",gray)
+cv2.waitKey(0)#wait for the key event
+cv2.destroyAllWindows()
