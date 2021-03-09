b757b7b5e8b5d7ef0e79ea9a89ab77e1d69622e8
Avishake Maji <56298861+Avishake007@users.noreply.github.com>
2021-03-09 12:45:13 +0530

    Update color_to_gray.py

--- ChangeColorSpace/color_to_gray.py
+++ ChangeColorSpace/color_to_gray.py
@@ -1,11 +1,12 @@
-from PIL import Image, ImageOps 
-#Enter the path 
-path=input("Enter the path : ")
-#Open the image 
-im1 = Image.open(path) 
-#Convert the color image to Gray Scale 
-im2 = ImageOps.grayscale(im1) 
- #Display the image
-im2.show()
-#Save the image in the desired location
-im2.save("gray_image.png")
+import cv2
+path=input("Enter the path : ") 
+#Read the image
+image = cv2.imread(path)
+#Convert the color image to Gray Scale image
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#Displaying of Gray iamge
+cv2.imshow('Gray image', gray)
+#Saving of image
+cv2.imwrite("gray_scale.png",gray)
+cv2.waitKey(0)#wait for the key event
+cv2.destroyAllWindows()
