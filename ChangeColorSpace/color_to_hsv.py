d71c2acdb259dd7f4c0b35d39f7097b2c5c3b69f
Avishake Maji <56298861+Avishake007@users.noreply.github.com>
2021-03-09 12:51:26 +0530

    Update color_to_hsv.py

--- ChangeColorSpace/color_to_hsv.py
+++ ChangeColorSpace/color_to_hsv.py
@@ -3,10 +3,10 @@ path=input("Enter the path : ")
 #Read the image
 image = cv2.imread(path)
 #Convert the color image to HSV image
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+hsv= cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 #Displaying of HSV iamge
-cv2.imshow('HSV image', gray)
+cv2.imshow('HSV image', hsv)
 #Saving of image
-cv2.imwrite("hsv_image.png",gray)
+cv2.imwrite("hsv_image.png",hsv)
 cv2.waitKey(0)#wait for the key event
 cv2.destroyAllWindows()
