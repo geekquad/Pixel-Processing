3f558b689acd7871cde9547a2104935854ed42a9
aswarth123 <c.v.aswarth@gmail.com>
2021-03-16 19:40:54 +0530

    modularised the code

--- HarrisCornerDetection/Harris_Corner_Detection.py
+++ HarrisCornerDetection/Harris_Corner_Detection.py
@@ -1,17 +1,21 @@
 import cv2
 import numpy as np
 
-img_path = 'image.jpg'
-pic = cv2.imread(img_path)
-pic_gray = cv2.cvtColor(pic,cv2.COLOR_BGR2GRAY)
+def Harris_Corner_Detector(path):
+    pic = cv2.imread(img_path)
+    pic_gray = cv2.cvtColor(pic,cv2.COLOR_BGR2GRAY)
 
-pic_arr = np.float32(pic_gray)
-#print(pic_arr)
-dst = cv2.cornerHarris(pic_arr,3,5,0.02)
-dst = cv2.dilate(dst,None)
+    pic_arr = np.float32(pic_gray)
+    #print(pic_arr)
+    dst = cv2.cornerHarris(pic_arr,3,5,0.02)
+    dst = cv2.dilate(dst,None)
 
-pic[dst>0.02*dst.max()]=[255,0,0]
+    pic[dst>0.02*dst.max()]=[255,0,0]
+    return(pic)
 
-cv2.imshow('Corners',pic)
-if cv2.waitKey(0) & 0xff == 27:
-    cv2.destroyAllWindows()
+if __name__=="__main__":
+    img_path = 'image.jpg'
+    Harris_Corner_Detector(img_path)
+    cv2.imshow('Corners',pic)
+    if cv2.waitKey(0) & 0xff == 27:
+        cv2.destroyAllWindows()
